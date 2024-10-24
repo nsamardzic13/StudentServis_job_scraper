dac610d1b3d9041402df543c9ed2a66f5c7fc19c
Nikola Samardzic <nikola.samardzic1997@gmail.com>
2024-10-24 10:51:41 +0200

    Fixed timezone

--- infrastructure/cloudrun.tf
+++ infrastructure/cloudrun.tf
@@ -20,7 +20,8 @@ resource "google_cloud_run_v2_job" "cloud_run" {
 resource "google_cloud_scheduler_job" "cloud_run_scheduler" {
   name     = "${var.project_name}-scheduler"
   project  = var.gcp_project
-  schedule = "45 8 * * 1-5" # 3 PM UTC every weekday (Monday to Friday)
+  schedule = "55 8 * * 1-5" # 3 PM UTC every weekday (Monday to Friday)
+  time_zone = "Europe/Belgrade"
 
   http_target {
     http_method = "POST"
@@ -29,6 +30,4 @@ resource "google_cloud_scheduler_job" "cloud_run_scheduler" {
       service_account_email = google_service_account.service_account.email
     }
   }
-
-  time_zone = "UTC"
 }
