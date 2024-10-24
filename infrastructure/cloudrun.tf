a5f349af870db91feeb81a45aa7c6198b945a599
Nikola Samardzic <nikola.samardzic1997@gmail.com>
2024-10-24 10:25:21 +0200

    Convert service to job

--- infrastructure/cloudrun.tf
+++ infrastructure/cloudrun.tf
@@ -1,36 +1,30 @@
-resource "google_cloud_run_v2_service" "cloud_run" {
+resource "google_cloud_run_v2_job" "cloud_run" {
   name     = "${var.project_name}-cloudrun"
   location = var.region
-  ingress  = "INGRESS_TRAFFIC_INTERNAL_ONLY"
-
   template {
-    containers {
-      image = var.image_name
-      ports {
-        container_port = 8080
-      }
-      resources {
-        limits = {
-          "cpu"    = "1"
-          "memory" = "512Mi"
+    template {
+      containers {
+        image = var.image_name
+        resources {
+          limits = {
+            "cpu"    = "1"
+            "memory" = "512Mi"
+          }
         }
       }
+      service_account = google_service_account.service_account.email
     }
-    scaling {
-      max_instance_count = 1
-    }
-    service_account = google_service_account.service_account.email
   }
 }
 
 resource "google_cloud_scheduler_job" "cloud_run_scheduler" {
   name     = "${var.project_name}-scheduler"
   project  = var.gcp_project
-  schedule = "20 8 * * 1-5" # 3 PM UTC every weekday (Monday to Friday)
+  schedule = "30 8 * * 1-5" # 3 PM UTC every weekday (Monday to Friday)
 
   http_target {
     http_method = "POST"
-    uri         = google_cloud_run_v2_service.cloud_run.uri
+    uri         = "https://${var.region}-run.googleapis.com/v2/projects/${var.gcp_project}/locations/${var.region}/jobs/${google_cloud_run_v2_job.cloud_run.name}:run"
     oidc_token {
       service_account_email = google_service_account.service_account.email
     }
