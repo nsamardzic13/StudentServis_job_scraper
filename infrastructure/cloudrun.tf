bac9e6648f973f4086c1e40e05220013b32baabe
Nikola Samardzic <nikola.samardzic1997@gmail.com>
2024-10-24 10:43:03 +0200

    Fixed cicd

--- infrastructure/cloudrun.tf
+++ infrastructure/cloudrun.tf
@@ -20,7 +20,7 @@ resource "google_cloud_run_v2_job" "cloud_run" {
 resource "google_cloud_scheduler_job" "cloud_run_scheduler" {
   name     = "${var.project_name}-scheduler"
   project  = var.gcp_project
-  schedule = "40 8 * * 1-5" # 3 PM UTC every weekday (Monday to Friday)
+  schedule = "45 8 * * 1-5" # 3 PM UTC every weekday (Monday to Friday)
 
   http_target {
     http_method = "POST"
